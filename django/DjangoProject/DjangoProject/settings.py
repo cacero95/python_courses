14650cdc094abb3eae8bae73b10bfe8c30549e0b
Carlos Acero <cacero95@gmail.com>
2020-08-04 13:57:49 -0500

    django the best in framework

--- django/DjangoProject/DjangoProject/settings.py
+++ django/DjangoProject/DjangoProject/settings.py
@@ -122,3 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#ckeditor settings
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source', 'Table', 'Image']
+        ]
+    }
+}
